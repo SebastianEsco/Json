17ff7c6f8f82fd9aaa1a0b52ec0c473e14eae3fc
Sebastián Escobar <Sebastian.escobarm@upb.edu.co>
2025-08-13 22:54:21 -0500

    Avances

--- Actividad2/Assets/Scripts/HttpTest.cs
+++ Actividad2/Assets/Scripts/HttpTest.cs
@@ -2,25 +2,94 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+using Input = UnityEngine.Windows.Input;
 
+
+[System.Serializable]
+public class PlayerData
+{
+    public int id;
+    public string username;
+    public bool state;
+    public List<int> deck;
+}
 public class HttpTest : MonoBehaviour
 {
-    [SerializeField]
-    private int characterId = 1;
+    private string APIUrl = "https://my-json-server.typicode.com/SebastianEsco/Json/users/";
+    private string RicKandMortyUrl = "https://rickandmortyapi.com/api/character/";
 
     [SerializeField]
-    private string url = "https://rickandmortyapi.com/api/character/";
+    private RawImage[] rawImage;
+    [SerializeField] private TextMeshProUGUI id;
 
-    [SerializeField] private RawImage rawImage;
     void Start()
     {
-        url = "https://rickandmortyapi.com/api/character/" + characterId;
-        StartCoroutine(GetText());
+        
     }
 
-    IEnumerator GetText()
+    public void NuevoUsuario()
     {
-        UnityWebRequest request = UnityWebRequest.Get(url);
+        string usuario = id.text.Trim();
+
+        if (int.TryParse(usuario, out int userId))
+        {
+            StartCoroutine(GetUser(userId)); 
+        }
+        else
+        {
+            Debug.LogError("El valor ingresado no es un ID válido: '" + usuario + "'");
+        }
+    }
+
+    IEnumerator GetUser(int userId)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(APIUrl+userId);
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            if (request.responseCode == 200)
+            {
+                // Show results as text
+                string json = request.downloadHandler.text;
+                Debug.Log(json);
+                
+                PlayerData player = JsonUtility.FromJson<PlayerData>(json);
+                
+                List<int> deckList = player.deck;
+                
+                int idUsuario = player.id;
+                
+                Debug.Log(idUsuario);
+
+                int i = 0;
+                foreach (int cardId in deckList)
+                {
+                    Debug.Log("Carta: " + cardId);
+                    StartCoroutine(GetCharacter(cardId, i));
+                    i++;
+                }
+                
+                
+
+            }
+            else
+            {
+                string mensaje = "status:" + request.responseCode;
+                mensaje += "\nError: " + request.error;
+                Debug.Log(mensaje);
+            }
+           
+        }
+    }
+    IEnumerator GetCharacter(int characterId, int imagePosition)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(RicKandMortyUrl+characterId);
         yield return request.SendWebRequest();
 
         if (request.result == UnityWebRequest.Result.ConnectionError)
@@ -35,6 +104,7 @@ public class HttpTest : MonoBehaviour
                 string json = request.downloadHandler.text;
                 Character character = JsonUtility.FromJson<Character>(json);
                 Debug.Log(character.id + ":" + character.name + " is an " + character.species);
+                StartCoroutine(GetImage(character.image, imagePosition));
             }
             else
             {
@@ -45,16 +115,34 @@ public class HttpTest : MonoBehaviour
            
         }
     }
-    
-    IEnumerator GetImage(string imageUrl)
+
+    IEnumerator GetImage(string imageUrl, int imagePosition)
     {
-        UnityWebRequest request = UnityWebRequest.Get(imageUrl);
-        
-        
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(imageUrl);
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            if (request.responseCode == 200)
+            {
+                // Show results as texture
+                Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                Debug.Log("Image downloaded successfully");
+                rawImage[imagePosition].texture = texture; 
+            }
+            else
+            {
+                string mensaje = "status:" + request.responseCode;
+                mensaje += "\nError: " + request.error;
+                Debug.Log(mensaje);
+            }
+        }
     }
+    
 }
-
-
 
 public class Character
 {
@@ -63,3 +151,5 @@ public class Character
     public string species;
     public string image;
 }
+
+ 
